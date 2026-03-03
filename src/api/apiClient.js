a470d8ccaf6bdca76295d7ba9d337f003cfe4ef6
akshay tr <akshaytr31@gmail.com>
2026-03-03 12:12:29 +0530

    production safe

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const API_BASE_URL = ""; // Forced empty for Vite proxy
+let API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+if (API_BASE_URL.endsWith("/")) {
+  API_BASE_URL = API_BASE_URL.slice(0, -1);
+}
 console.log("Using API_BASE_URL:", API_BASE_URL);
+
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
 });
